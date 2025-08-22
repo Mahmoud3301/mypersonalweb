aeec5fce2aaaafff6b9d52a01860237fa6a2c412
Mahmoud3301 <mahmoudsaidmansour1000@gmail.com>
2025-08-23 01:44:34 +0300

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/myweb/', // لازم يتطابق مع اسم الريبو
+  base: '/myweb/'  // لازم يتطابق مع اسم الريبو
 })
