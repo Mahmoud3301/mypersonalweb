182af795bf989150f5f51a9138886b523504879f
Mahmoud3301 <mahmoudsaidmansour1000@gmail.com>
2025-08-23 02:52:22 +0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // لازم يتطابق مع اسم الريبو (myweb)
 export default defineConfig({
   plugins: [react()],
-  base: '/myweb/',
+  base: '/mypersonalweb/',
 })
 
