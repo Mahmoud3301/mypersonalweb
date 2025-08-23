fcc3106f5f491b25bf42973b5822d8352e0dbfa8
MahmoudSaeed <151455428+Mahmoud3301@users.noreply.github.com>
2025-08-23 05:16:26 +0300

    Add files via upload

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -56,7 +56,7 @@ function Home() {
         <p className="download-msg">
           If you want to download my CV, visit my{' '}
           <a
-            href="https://www.linkedin.com/in/your-linkedin-profile/"
+            href="https://www.linkedin.com/in/mahmoud-mansour-928623252/"
             target="_blank"
             rel="noopener noreferrer"
           >
